3f3b41fa35ed980577434eec55d196078545b7e2
TyrannosaurusRhett <112954916+TyrannosaurusRhett@users.noreply.github.com>
2022-09-27 17:44:34 -0400

    Add files via upload

---
+++ chap2assign/myFavMovie.js
@@ -0,0 +1,9 @@
+//Taylor Rhett
+
+document.write("When a boy falls ill, his grandfather comes to visit to cheer him up with a storybook called The Princess Bride. ")
+document.write("It tells the tale of Westley and Buttercup, who fall in love but are separated due to unfortunate circumstances and Westley’s seeming death at the hands of an infamous pirate known for never taking prisoners. ")
+document.write("Five years later Buttercup is chosen to be wed to the prince of the country despite not loving him, but she is kidnapped in what turns out to be a ploy to start a war between her country and a neighboring one. ")
+document.write("As she is taken away, a mysterious masked man dressed all in black begins to pursue Buttercup, fighting and besting each of her kidnappers until finally he is able to rescue her and is revealed to be Westley, who did not die in the pirate attack. ")
+document.write("The two are parted again, however, when the prince finds them, and Buttercup offers to go with him in order to save Westley’s life. ")
+document.write("He is nearly killed anyway by the prince’s right hand man and torturer, but he is rescued and revived by two of the kidnappers he fought earlier and spared from death because they think he might be a means to achieving their own goals. ")
+document.write("Together the trio mount a rescue operation to save Buttercup from the prince, who still wishes to kill her as a pretense for going to war, and after a final sword fight and an amazing bluff by the still half-dead Westley, the group manages to succeed and escape. ")
